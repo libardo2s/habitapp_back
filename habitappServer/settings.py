0b1fc8b00a0b2077f8bd854f980af7a3cc6c3062
Libardo <libardoii@hotmail.com>
2020-09-09 11:26:35 -0500

    setting server

--- habitappServer/settings.py
+++ habitappServer/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'habitappServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -91,7 +92,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
